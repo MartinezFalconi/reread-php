e984bfe45985ae0a4fa1a99b73cefca77fe6b27f
MartinezFalconi <sergiomafa09@gmail.com>
2020-09-25 18:37:22 +0200

    new

---
+++ services/connection.php
@@ -0,0 +1,20 @@
+<?php
+// Estilo por procedimientos
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_reread"
+
+// Crear la conexion
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+// Checkear la conexion
+if (!$conn) {
+    echo "Error: No se pudo conectar a MySQL" . PHP_EQL;
+    echo "Error de depuración: " . mysqli_connect_erno() . PHP_EQL;
+    exit;
+} else {
+    mysqli_set_charset($conn, "utf8");
+}
+
