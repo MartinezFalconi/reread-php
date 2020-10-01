20ece15470731b283edfde56070a426d1e309870
MartinezFalconi <sergiomafa09@gmail.com>
2020-10-01 17:47:23 +0200

    NEXT

--- view/ebooks.php
+++ view/ebooks.php
@@ -28,6 +28,40 @@
       <a href="ebooks.php">eBooks</a>
     </div>
     <h3>Toda la actualidad en eBook</h3>
+
+    <!-- NUEVO DESARROLLO: FORM-->
+
+    <div>
+      <form action="ebooks.php" method="POST">
+        <label for="fautor">Autor</label>
+        <input type="text" id="fautor" name="fautor" placeholder="Introduce el autor...">
+        <!--         
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="country">Country</label>
+        <select id="country" name="country">
+          <option value="australia">Australia</option>
+          <option value="canada">Canada</option>
+          <option value="usa">USA</option>
+        </select>
+        -->
+      
+        <input type="submit" value="Buscar">
+      </form>
+    </div>
+
+    <?php
+
+    if(isset($_POST['fautor'])){
+      //filtrara los ebooks que se mostraran en la pagina.
+    }else{
+      //si no mostrara todos los ebooks de la database
+    }
+
+    
+
+    ?>
     
       <!--<div class="ebook"> 
         <a href="https://www.amazon.es/Cell-BEST-SELLER-Stephen-King/dp/8483465213"><img src="../img/ebook1.jpeg" alt="ebook 1">
